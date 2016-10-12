f17876df19b0c424192c3b875bb3766a315d53d3
TheRaptors <Mcsys@hotmail.com>
2016-10-12 20:23:02 +0800

    2016-10-12 20:22:59

---
+++ WebApps/models.py
@@ -0,0 +1,29 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class UserType(models.Model):
+    Type = models.CharField(max_length = 50)
+
+class UserInfo(models.Model):
+    username = models.CharField(max_length = 50)
+    password = models.CharField(max_length = 50)
+    Gender = models.BooleanField(default = False)
+    Age = models.IntegerField(default = 18)
+    Memo = models.TextField(default = 'Memo')
+    CreateDate = models.DateField(default = '2016-10-01')
+    CreateTime = models.DateTimeField(default = '2016-10-01 10:10:10')
+    #One To Many
+    Type = models.ForeignKey('UserType')
+
+class Group(models.Model):
+    GroupName = models.CharField(max_length = 50)
+
+class User(models.Model):
+    UserName = models.CharField(max_length = 50)
+    Email = models.CharField(max_length = 50)
+    #Many To Many
+    GroupRelation = models.ManyToManyField('Group')
+    #One To One

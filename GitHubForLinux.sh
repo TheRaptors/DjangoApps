a39736c7fc31f5f70af498a1bc028f6305889231
TheRaptors <Mcsys@hotmail.com>
2016-10-15 10:46:12 +0800

    2016-10-15 10:46:12

--- GitHubForLinux.sh
+++ GitHubForLinux.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-git add -A .
-
-git commit -m "$(date +%Y-%m-%d\ %H:%M:%S)"
-
-git push DjangoApps master
+if [ ! -d .git ];then
+    echo 'Warning:The Work Directory Is Wrong. Please Check It and Retry!'
+else
+    git add -A .
+    git commit -m "$(date +%Y-%m-%d\ %H:%M:%S)"
+    git push DjangoApps master
+fi

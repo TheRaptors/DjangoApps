a8fde3f152097642dd9423480b679e8bd96b507a
TheRaptors <Mcsys@hotmail.com>
2016-10-22 11:05:53 +0800

    2016-10-22 11:05:53

--- GitHubForLinux.sh
+++ GitHubForLinux.sh
@@ -4,7 +4,9 @@ if [ ! -d .git -a ! -f manage.py ];then
     echo 'Warning:The Work Directory Is Wrong. Please Check It And Retry!'
     exit
 else
+    PWD=`pwd`
+    Path=`basename ${PWD}`
     git add -A .
     git commit -m "$(date +%Y-%m-%d\ %H:%M:%S)"
-    git push DjangoApps master
+    git push ${Path} master
 fi

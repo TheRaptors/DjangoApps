e5abb44c009d291d181d21e3d4bdd3c75c291b1f
TheRaptors <Mcsys@hotmail.com>
2016-10-22 09:56:26 +0800

    2016-10-22 09:56:26

--- GitHubForLinux.sh
+++ GitHubForLinux.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 if [ ! -d .git -a ! -f manage.py ];then
-    echo 'Warning:The Work Directory Is Wrong. Please Check It and Retry!'
+    echo 'Warning:The Work Directory Is Wrong. Please Check It And Retry!'
+    exit
 else
     git add -A .
     git commit -m "$(date +%Y-%m-%d\ %H:%M:%S)"

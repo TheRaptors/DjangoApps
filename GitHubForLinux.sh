45d9480fbee118ff87032e612e7a0ed35ba5cc34
TheRaptors <Mcsys@hotmail.com>
2016-10-22 15:24:39 +0800

    2016-10-22 15:24:39

--- GitHubForLinux.sh
+++ GitHubForLinux.sh
@@ -4,9 +4,7 @@ if [ ! -d .git -a ! -f manage.py ];then
     echo 'Warning:The Work Directory Is Wrong. Please Check It And Retry!'
     exit
 else
-    PWD=`pwd`
-    Path=`basename ${PWD}`
     git add -A .
     git commit -m "$(date +%Y-%m-%d\ %H:%M:%S)"
-    git push ${Path} master
+    git push $(basename `pwd`) master
 fi
